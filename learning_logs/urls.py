848e6e89c999a16ec76388d2c01983481152fa79
hagikehappy <hagikehappy@163.com>
2023-10-07 23:56:22 +0800

    After Edit Entry

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path('topics/<int:topic_id>/', views.topic, name='topic'),
     path('new_topic/', views.new_topic, name='new_topic'),
     path('new_entry/<int:topic_id>', views.new_entry, name='new_entry'),
+    # 用于编辑条目的页面
+    path('edit_entry/<int:entry_id>', views.edit_entry, name='edit_entry'),
 ]
